5d1a2b3175b0e8434080d314c0059628272308eb
cthuff <cthuff03@gmail.com>
2018-02-27 20:37:43 -0800

    coding files

---
+++ Homework2/src/GridFileManager.java
@@ -0,0 +1,15 @@
+public class GridFileManager {
+
+    GridFileManager(String databaseName) {
+
+    }
+    //boolean
+     void createGridFile(String fileName, int lowX, int highX, int numLinesX, int lowY, int highY, int numLinesY, int numBuckets) {
+
+    }
+    //GridRecord[]
+    void lookup(String fileName, GridPoint pt1, GridPoint pt2, int limit_offset, int limit_count){
+
+
+    }
+}
---
+++ Homework2/src/GridRecord.java
@@ -0,0 +1,19 @@
+public class GridRecord
+{
+    public String label;
+    public GridPoint point;
+    public GridRecord(String label, GridPoint point)
+    {
+        this.label = label;
+        this.point = point;
+    }
+    public GridRecord(String label, float x, float y)
+    {
+        this.label = label;
+        this.point = new GridPoint(x, y);
+    }
+    public String toString()
+    {
+        return label + point.toString();
+    }
+}
