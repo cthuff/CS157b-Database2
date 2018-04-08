6809b39a61269660b8eb3d5785e5b5e0b9c1ec5c
cthuff <cthuff03@gmail.com>
2018-04-08 16:08:45 -0700

    Homework3

--- Homework3/Homework3/src/DataGenerator.java
+++ Homework3/Homework3/src/DataGenerator.java
@@ -1,7 +1,25 @@
+import java.io.*;
+
 public class DataGenerator {
 
-    public static void main (String args[])
-    {
-        System.out.println("Hello World");
+    public static void makeData(int startNumber, int numberRows, int wrapNumber, String fileName) {
+        try {
+            PrintStream outFile = new PrintStream(new FileOutputStream(fileName));
+            int column = 1;
+            int endNumber = startNumber + numberRows;
+            String row;
+            for (int columnA = startNumber; columnA < endNumber; columnA++) {
+                row = "" + columnA + " " + column;
+                outFile.println(row);
+                column = (column >= wrapNumber) ? 1 : column + (int)(Math.random()*wrapNumber);
+            }
+            outFile.close();
+        } catch (Exception ie) {
+            ie.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        makeData(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]), args[3]);
     }
 }
